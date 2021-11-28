bd79c5187a961ebe835cf7bd048d082e5b5ad904
Vlad Yacishin <d_m_n04@mail.ru>
2021-11-29 00:48:46 +0300

    Get posts with count and order params

--- ApiPost.php
+++ ApiPost.php
@@ -6,16 +6,24 @@ namespace sd;
 
 class ApiPost {
 	protected $apiBaseUrl;
+	protected $count = 20;
+	protected $order = 'DESC';
 
-	public function __construct() {
-		$this->setSettings();
+	public function __construct( $options ) {
+		$this->setSettings( $options );
 	}
 
-	protected function setSettings() {
+	protected function setSettings( $options ) {
 		$this->apiBaseUrl = 'http://simplyd.beget.tech/';
+		if ( isset( $options['newsCount'] ) && is_numeric( $options['newsCount'] ) && $options['newsCount'] > 0 ) {
+			$this->count = (int) $options['newsCount'];
+		}
+		if ( isset( $options['newsOrder'] ) && ! empty( $options['newsOrder'] ) && $options['newsOrder'] === 'ASC' ) {
+			$this->order = $options['newsOrder'];
+		}
 	}
 
 	public function getPosts() {
-		return file_get_contents( $this->apiBaseUrl );
+		return file_get_contents( "{$this->apiBaseUrl}?count={$this->count}&order={$this->order}" );
 	}
 }
