79949fed359df73b9a7a148cbdbab540d14e348c
Vlad Yacishin <d_m_n04@mail.ru>
2021-11-29 01:17:01 +0300

    Add base styles

--- WP-plugin-work-with-api.php
+++ WP-plugin-work-with-api.php
@@ -9,3 +9,13 @@ require_once 'ApiPost.php';
 require_once 'Settings.php';
 
 new \sd\Settings();
+global $pagenow;
+
+/* Enqueue only for current page */
+if ( $pagenow === 'options-general.php' && isset( $_GET['page'] ) && $_GET['page'] === 'sd-news' ) {
+	add_action( 'admin_enqueue_scripts', 'sdPluginStyles' );
+}
+
+function sdPluginStyles() {
+	wp_enqueue_style( 'sd-plugin-styles-main', plugin_dir_url( __FILE__ ) . 'assets/css/main.css', null, filemtime( plugin_dir_path( __FILE__ ) . 'assets/css/main.css' ) );
+}
